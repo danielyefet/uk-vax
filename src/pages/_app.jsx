3a077d28c4a02479b7975ad828281562cac0afc1
danielyefet <daniel.yefet@gmail.com>
2021-01-31 22:28:57 +0000

    Add icons

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,27 @@
 import "tailwindcss/tailwind.css";
+import Image from 'next/image';
 
 function App({ Component, pageProps }) {
     return(
-        <Component {...pageProps} />
+        <>
+            <Component {...pageProps} />
+            <footer className="flex justify-center pb-10">
+                <a href="https://github.com/danielyefet/uk-vax" className="mr-2 opacity-20 hover:opacity-50">
+                    <Image
+                        src="/github.png"
+                        width="20"
+                        height="20"
+                    />
+                </a>
+                <a href="https://twitter.com/danyefet" className="opacity-20 hover:opacity-50">
+                    <Image
+                        src="/twitter.png"
+                        width="20"
+                        height="20"
+                    />
+                </a>
+            </footer>
+        </>
     );
 }
 
