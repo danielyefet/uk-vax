d6835816290f948c7f5b19fc38ea61df872d05b9
danielyefet <daniel.yefet@gmail.com>
2021-01-31 18:55:17 +0000

    Increase padding

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,7 +10,7 @@ function Index({ totalVaccinations }) {
             <Head>
                 <title>COVID - Percentage Vaccinated - United Kingdom</title>
             </Head>
-            <div className="m-auto max-w-lg p-3">
+            <div className="m-auto max-w-lg p-6">
                 <h1 className="text-center text-3xl pt-6 pb-3 text-gray-600">Coronavirus Vaccine Status (UK)</h1>
                 <section className="grid grid-cols-1 md:grid-cols-2 min-h-full">
                     <div className="flex justify-center p-3">
