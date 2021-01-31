d9cf58e86daa567577e781dde820d18c553d2483
danielyefet <daniel.yefet@gmail.com>
2021-01-31 22:48:28 +0000

    Add favicon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,21 +1,31 @@
 import "tailwindcss/tailwind.css";
 import Image from 'next/image';
+import Head from 'next/head';
 
 function App({ Component, pageProps }) {
     return(
         <>
+            <Head>
+                <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+                <meta name="msapplication-TileColor" content="#da532c" />
+                <meta name="theme-color" content="#ffffff" />
+            </Head>
             <Component {...pageProps} />
             <footer className="flex justify-center pb-10">
                 <a href="https://github.com/danielyefet/uk-vax" className="mr-2 opacity-20 hover:opacity-50">
                     <Image
-                        src="/github.png"
+                        src="/icons/github.png"
                         width="20"
                         height="20"
                     />
                 </a>
                 <a href="https://twitter.com/danyefet" className="opacity-20 hover:opacity-50">
                     <Image
-                        src="/twitter.png"
+                        src="/icons/twitter.png"
                         width="20"
                         height="20"
                     />
